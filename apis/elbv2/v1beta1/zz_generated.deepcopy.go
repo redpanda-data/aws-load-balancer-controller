9f40bdfdcb8ebe6ba7c80ce920b1a3ad06335e74
Olivia Song <sonyingy@amazon.com>
2024-02-19 13:49:33 -0800

    bump up controller-gen version and update manifests (#3580)

--- apis/elbv2/v1beta1/zz_generated.deepcopy.go
+++ apis/elbv2/v1beta1/zz_generated.deepcopy.go
@@ -310,7 +310,8 @@ func (in *SubnetSelector) DeepCopyInto(out *SubnetSelector) {
 			if val == nil {
 				(*out)[key] = nil
 			} else {
-				in, out := &val, &outVal
+				inVal := (*in)[key]
+				in, out := &inVal, &outVal
 				*out = make([]string, len(*in))
 				copy(*out, *in)
 			}
