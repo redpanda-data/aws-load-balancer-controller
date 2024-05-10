48b97c55fc847a5076f7ed7b152544f7c160ded5
Shraddha Bang <18206078+shraddhabang@users.noreply.github.com>
2024-05-10 13:52:28 -0700

    Add non-alphanumeric input validation test for vpcID and fix typo (#3687)
    * Add non-alphanumeric input validation test for vpcID
    
    * Fix cert arn typo and add docs for cert arn support in IngressClassParams

--- pkg/ingress/model_builder_test.go
+++ pkg/ingress/model_builder_test.go
@@ -1895,7 +1895,7 @@ func Test_defaultModelBuilder_Build(t *testing.T) {
 							IngClassConfig: ClassConfiguration{
 								IngClassParams: &v1beta1.IngressClassParams{
 									Spec: v1beta1.IngressClassParamsSpec{
-										CertficateArn: []string{"arn:aws:acm:us-east-1:9999999:certificate/ingress-class-certificate-arn"},
+										CertificateArn: []string{"arn:aws:acm:us-east-1:9999999:certificate/ingress-class-certificate-arn"},
 									},
 								},
 							},
