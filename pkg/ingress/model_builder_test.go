6ab8a72d9a2d159aa301c49c6de7d8e3cac805ef
Gauthier Delacroix <gauthier_delacroix@connect-tech.sncf>
2025-05-20 11:06:40 +0200

    Fix IngressClassParams doc instead

--- pkg/ingress/model_builder_test.go
+++ pkg/ingress/model_builder_test.go
@@ -4021,7 +4021,7 @@ func Test_defaultModelBuilder_Build(t *testing.T) {
 							IngClassConfig: ClassConfiguration{
 								IngClassParams: &v1beta1.IngressClassParams{
 									Spec: v1beta1.IngressClassParamsSpec{
-										PrefixListIDs: []string{
+										PrefixListsIDs: []string{
 											"pl-11111111",
 											"pl-22222222",
 										},
