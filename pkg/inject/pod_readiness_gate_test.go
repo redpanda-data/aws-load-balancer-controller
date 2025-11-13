5a7ee5c32ea8a9553a1ca01c60e8f6c3200d5664
Zachary Nixon <nixozach@amazon.com>
2025-11-13 11:55:27 -0800

    Revert "Znixon/quic2 (#4447)"
    This reverts commit b657107e083b386263f3f12a381482ba52395ce9.

--- pkg/inject/pod_readiness_gate_test.go
+++ pkg/inject/pod_readiness_gate_test.go
@@ -1,4 +1,4 @@
-package pod_readiness
+package inject
 
 import (
 	"context"
@@ -120,7 +120,7 @@ func Test_PodReadinessGate_Mutate(t *testing.T) {
 		tgbList   []*elbv2api.TargetGroupBinding
 		pod       *corev1.Pod
 		want      []corev1.PodReadinessGate
-		config    PodReadinessGateConfig
+		config    Config
 		wantError bool
 	}{
 		{
@@ -142,7 +142,7 @@ func Test_PodReadinessGate_Mutate(t *testing.T) {
 					ConditionType: "target-health.elbv2.k8s.aws/tgb-1-l6qw1",
 				},
 			},
-			config: PodReadinessGateConfig{
+			config: Config{
 				EnablePodReadinessGateInject: true,
 			},
 		},
@@ -161,7 +161,7 @@ func Test_PodReadinessGate_Mutate(t *testing.T) {
 				},
 			},
 			want: nil,
-			config: PodReadinessGateConfig{
+			config: Config{
 				EnablePodReadinessGateInject: true,
 			},
 		},
@@ -187,7 +187,7 @@ func Test_PodReadinessGate_Mutate(t *testing.T) {
 					ConditionType: "target-health.elbv2.k8s.aws/tgb-2-l6qw2",
 				},
 			},
-			config: PodReadinessGateConfig{
+			config: Config{
 				EnablePodReadinessGateInject: true,
 			},
 		},
@@ -204,7 +204,7 @@ func Test_PodReadinessGate_Mutate(t *testing.T) {
 				},
 			},
 			want: []corev1.PodReadinessGate(nil),
-			config: PodReadinessGateConfig{
+			config: Config{
 				EnablePodReadinessGateInject: true,
 			},
 		},
@@ -232,7 +232,7 @@ func Test_PodReadinessGate_Mutate(t *testing.T) {
 					ConditionType: "leave-unmodified",
 				},
 			},
-			config: PodReadinessGateConfig{
+			config: Config{
 				EnablePodReadinessGateInject: true,
 			},
 		},
@@ -249,7 +249,7 @@ func Test_PodReadinessGate_Mutate(t *testing.T) {
 				},
 			},
 			want: []corev1.PodReadinessGate(nil),
-			config: PodReadinessGateConfig{
+			config: Config{
 				EnablePodReadinessGateInject: true,
 			},
 		},
@@ -300,7 +300,7 @@ func Test_PodReadinessGate_Mutate(t *testing.T) {
 					ConditionType: "target-health.elbv2.k8s.aws/tgb-2-l6qw2",
 				},
 			},
-			config: PodReadinessGateConfig{
+			config: Config{
 				EnablePodReadinessGateInject: true,
 			},
 		},
